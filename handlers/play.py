2e0353372227be95a8cd2da25613f421f29fcb09
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2023-12-30 19:05:53 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -420,7 +420,7 @@ async def play(_, message: Message):
                       if administrator == message.from_user.id:  
                           try:
                               invitelink = await _.export_chat_invite_link(chat_id)
-                            if invitelink.startswith("https://t.me/+"):
+                            if invitelink.startswith("https://t.me/+")
                                 invitelink = invitelink.replace("https://t.me/+","https://t.me/joinchat/")
                           except:
                               await lel.edit(
